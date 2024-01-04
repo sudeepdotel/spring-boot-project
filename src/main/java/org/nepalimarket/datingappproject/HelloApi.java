ff6aa715f71ac213822ebac733bf03d0d2b4aadf
sudeep-macmini <sudeepdotel1990@gmail.com>
2024-01-03 18:10:01 -0600

    Updated helloApi

--- src/main/java/org/nepalimarket/datingappproject/HelloApi.java
+++ src/main/java/org/nepalimarket/datingappproject/HelloApi.java
@@ -4,11 +4,7 @@ package org.nepalimarket.datingappproject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -17,7 +13,19 @@ public class HelloApi {
     @GetMapping("/hello")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> helloApi() {
-        String message = "Welcome to the Hello API!";
+        String message = """
+                <h2>Welcome to the Hello API!</h2>
+                <p>Here's a short to-do list for dating app:</p>
+                <ol>
+                   <li>Create user registration and login functionality.</li>
+                   <li>Integrate Facebook API for social media features.</li>
+                   <li>Implement a matching algorithm for potential matches.</li>
+                   <li>Design and implement user profiles and profile management.</li>
+                   <li>Create a messaging system for matched users.</li>
+                </ol>
+                <p>customize and expand this list based on  project requirements!</p>
+                """;
+
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
